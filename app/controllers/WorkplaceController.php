518d182d7e1016003d0ced4b1705e126851a1b05
meriial <cameron@twosmiles.ca>
2014-05-10 15:51:19 -0600

    clean up

--- app/controllers/WorkplaceController.php
+++ app/controllers/WorkplaceController.php
@@ -11,68 +11,12 @@ class WorkplaceController extends \BaseController {
 	{
 		$skip = Input::get('skip');
 
-		$r = DB::table( 'D3_Viz_data' )
+		$response = DB::table( 'D3_Viz_data' )
 			->select( 'x', 'y', 'r', 'label' )
 			->where( 'Name', "co2saved" )
 			->get();
 			
-		return Response::json( $r );
-		
-		
-		
-		
-		echo '<pre>';
-		var_dump($r);
-		echo '</pre>';
-		die();
-
-		$response = [];
-		
-		$p_count = [];
-		
-		$km = $this->getKm();
-			
-		$p_count = DB::table('registrant')
-			->join( 'workplace', 'registrant.workplace_id', '=', 'workplace.workplace_id' )
-			->select( DB::raw('count(registrant.registrant_id) as y, workplace.workplace_id as id') )
-			->groupBy( 'workplace_name' )
-			->take( 50 )
-			->get();
-			
-		foreach( $p_count as $p ) {
-			if( !isset( $km[$p->id] ) ) {
-				$km[$p->id] = new stdClass();
-			}
-			if( isset( $km[$p->id]->workplace_headcount ) && $km[$p->id]->workplace_headcount != 0 ) {
-				$km[$p->id]->y = $p->y / $km[$p->id]->workplace_headcount;
-				unset( $km[$p->id]->workplace_headcount );
-			} else {
-				$km[$p->id]->y = 0;
-			}
-			unset( $km->id );
-		}
-		
-		$response = $km;
-				
-		return Response::json($response);
-	}
-
-	private function getKm()
-	{
-		$kms = DB::table('commute')
-			->join( 'registrant', 'registrant.registrant_id', '=', 'commute.registrant_id' )
-			->join( 'workplace', 'registrant.workplace_id', '=', 'workplace.workplace_id' )
-			->select( DB::raw('sum(commute.distance_km) as x, workplace_name as label, workplace_headcount, workplace_headcount as r, workplace.workplace_id as id') )
-			->groupBy( 'workplace_name' )
-			->take( 50 )
-			->get();
-			
-		$a = [];
-		foreach( $kms as $km ) {
-			$a[$km->id] = $km;
-		}
-		
-		return $a;		
+		return Response::json( $response );
 	}
 
 	/**
@@ -107,9 +51,7 @@ class WorkplaceController extends \BaseController {
 	{
 		$workplace = Workplace::find($id);
 		
-		$response = $workplace->stats();
-		
-		return Response::json( $response );
+		return Response::json( $workplace );
 	}
 	
 	/**
